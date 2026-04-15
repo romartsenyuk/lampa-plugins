4e53d3c98769fa0ba13cb07ed5af6f4cefd07a79
romartsenyuk <romartsenyuk@gmail.com>
2026-04-15 21:11:24 +0300

    Update continue-sync.js

--- continue-sync.js
+++ continue-sync.js
@@ -1,43 +1,34 @@
 (function () {
     'use strict';
-
     var LampaSync = {
         name: 'Синхронізація+',
         init: function () {
             var _this = this;
-            
-            // Жорстке видалення всіх старих кнопок
             $('.js-sync-clean').remove();
             $('.menu__item').filter(function() {
-                var txt = $(this).text();
-                return txt.indexOf('Прогрес серій') > -1 || txt.indexOf('Синхронізація') > -1;
+                return $(this).text().indexOf('Прогрес серій') > -1 || $(this).text().indexOf('Синхронізація') > -1;
             }).remove();
 
             setTimeout(function(){ 
                 _this.addMenuItem();
-                _this.autoPull(); 
-            }, 1000);
+                _this.pull(true); 
+            }, 2000);
 
-            // Авто-збереження після перегляду
             Lampa.Player.listener.follow('destroy', function(){
-                setTimeout(function(){ _this.sync(true); }, 3000); 
+                setTimeout(function(){ _this.sync(true); }, 5000); 
             });
         },
-
         addMenuItem: function () {
             var _this = this;
             if ($('.js-sync-clean').length > 0) return;
-
             var item = $('<li class="menu__item selector focusable js-sync-clean"><div class="menu__ico" style="color: #ff9500 !important;"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4M17 8l-5-5-5 5M12 3v12"/></svg></div><div class="menu__text">' + this.name + '</div></li>');
             item.on('click', function() { _this.showSettings(); });
             $('.menu__list').append(item);
         },
-
         showSettings: function () {
             var _this = this;
             var key = localStorage.getItem('cs_key') || '';
             var bin = localStorage.getItem('cs_bin') || '';
-            
             Lampa.Select.show({
                 title: this.name,
                 items: [
@@ -60,15 +51,11 @@
                 }
             });
         },
-
         sync: function (silent) {
-            var key = localStorage.getItem('cs_key');
-            var bin = localStorage.getItem('cs_bin');
+            var key = localStorage.getItem('cs_key'), bin = localStorage.getItem('cs_bin');
             if (!key) return;
-            
             var data = Lampa.Storage.get('continue') || {};
-            if (!silent) Lampa.Noty.show('Синхронізація з хмарою...');
-            
+            if (!silent) Lampa.Noty.show('Синхронізація...');
             $.ajax({
                 url: bin ? 'https://api.jsonbin.io/v3/b/' + bin : 'https://api.jsonbin.io/v3/b',
                 type: bin ? 'PUT' : 'POST',
@@ -80,37 +67,32 @@
                 }
             });
         },
-
-        autoPull: function () {
-            var key = localStorage.getItem('cs_key');
-            var bin = localStorage.getItem('cs_bin');
-            if (key && bin) this.pull(true);
-        },
-
         pull: function (silent) {
-            var key = localStorage.getItem('cs_key');
-            var bin = localStorage.getItem('cs_bin');
+            var key = localStorage.getItem('cs_key'), bin = localStorage.getItem('cs_bin');
             if (!key || !bin) return;
-            
             $.ajax({
                 url: 'https://api.jsonbin.io/v3/b/' + bin + '/latest',
                 headers: { 'X-Master-Key': key },
                 success: function (res) {
                     if (res.record && res.record.backup) {
-                        // Примусовий запис у сховище Lampa
+                        // КЛЮЧОВИЙ МОМЕНТ ДЛЯ KINOSMOBILE:
+                        // Використовуємо внутрішній метод Lampa для запису з оповіщенням системи
                         Lampa.Storage.set('continue', res.record.backup);
                         
-                        // Команда для оновлення розділу "Продовжити перегляд"
-                        if (Lampa.Activity && Lampa.Activity.active().component === 'continue') {
-                             Lampa.Activity.active().activity.render();
+                        // Примусово оновлюємо кеш переглядів у самому додатку
+                        if (window.Lampa && Lampa.Continue) {
+                            Lampa.Continue.init(); 
                         }
-                        
-                        if (!silent) Lampa.Noty.show('Прогрес відновлено!');
+
+                        if (!silent) {
+                            Lampa.Noty.show('Дані відновлено!');
+                            // Повне перезавантаження для KinosMobile
+                            setTimeout(function(){ window.location.reload(); }, 500);
+                        }
                     }
                 }
             });
         }
     };
-
     LampaSync.init();
 })();
