e47001a3606f3929a050e08cd8ba572fc5369877
romartsenyuk <romartsenyuk@gmail.com>
2026-04-18 18:11:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,8 @@
             { title: 'Сяйво', url: 'https://syayvo.com' }
         ];
 
-        function openSwitch() {
+        // Функція виклику списку
+        var openMenu = function() {
             Lampa.Select.show({
                 title: 'Вибір сервера',
                 items: servers,
@@ -22,15 +23,19 @@
                     Lampa.Controller.toggle('menu');
                 }
             });
-        }
+        };
 
-        var btn = $('<li class="menu__item selector"><div class="menu__ico"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="12" cy="12" r="9" stroke="white" stroke-width="2"/></svg></div><div class="menu__text">Змінити хост</div></li>');
+        // Створення кнопки
+        var btn = $('<li class="menu__item selector"><div class="menu__ico"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><rect x="4" y="4" width="16" height="16" rx="2" stroke="white" stroke-width="2"/></svg></div><div class="menu__text">Змінити хост</div></li>');
         
         btn.on('hover:enter', function () {
-            openSwitch();
+            openMenu();
         });
 
-        $('.menu .menu__list').append(btn);
+        // Додаємо в меню з невеликою затримкою для надійності
+        setTimeout(function() {
+            $('.menu .menu__list').append(btn);
+        }, 1000);
     }
 
     if (window.appready) start();
