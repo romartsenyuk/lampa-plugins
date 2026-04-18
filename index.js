e53bdd425a3f17a89bd57768c82901444d406b5b
romartsenyuk <romartsenyuk@gmail.com>
2026-04-18 18:08:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,30 +7,27 @@
             { title: 'Сяйво', url: 'https://syayvo.com' }
         ];
 
-        Lampa.Component.add('sw_srv', function (object) {
-            var comp = new Lampa.InteractionMain(object);
-            comp.create = function () {
-                var _this = this;
-                servers.forEach(function (s) {
-                    var card = Lampa.Template.get('button', { title: s.title });
-                    card.on('hover:enter', function () {
-                        localStorage.setItem('host', s.url);
-                        location.href = s.url;
-                    });
-                    _this.append(card);
-                });
-            };
-            return comp;
-        });
+        function openSwitch() {
+            Lampa.Select.show({
+                title: 'Вибір сервера',
+                items: servers,
+                onSelect: function (item) {
+                    Lampa.Noty.show('Перехід на ' + item.title);
+                    localStorage.setItem('host', item.url);
+                    setTimeout(function () {
+                        location.href = item.url;
+                    }, 500);
+                },
+                onBack: function () {
+                    Lampa.Controller.toggle('menu');
+                }
+            });
+        }
 
-        var btn = $('<li class="menu__item selector"><div class="menu__ico"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><rect x="3" y="3" width="18" height="18" rx="2" stroke="white" stroke-width="2"/></svg></div><div class="menu__text">Змінити хост</div></li>');
+        var btn = $('<li class="menu__item selector"><div class="menu__ico"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="12" cy="12" r="9" stroke="white" stroke-width="2"/></svg></div><div class="menu__text">Змінити хост</div></li>');
         
         btn.on('hover:enter', function () {
-            Lampa.Activity.push({
-                title: 'Сервери',
-                component: 'sw_srv',
-                page: 1
-            });
+            openSwitch();
         });
 
         $('.menu .menu__list').append(btn);
