5535ae3b67d0c53210371ca9925859cd11618e14
romartsenyuk <romartsenyuk@gmail.com>
2026-04-18 18:15:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,26 @@
 (function () {
     'use strict';
-
     function start() {
-        var servers = [
-            { title: 'Lampa.mx', url: 'https://lampa.mx' },
-            { title: 'Сяйво', url: 'https://syayvo.com' }
+        var srv = [
+            { title: 'Lampa Main', url: 'https://lampa.mx' },
+            { title: 'Сяйво ТВ', url: 'https://syayvo.com' }
         ];
-
-        // Функція виклику списку
-        var openMenu = function() {
+        var open = function() {
             Lampa.Select.show({
-                title: 'Вибір сервера',
-                items: servers,
-                onSelect: function (item) {
-                    Lampa.Noty.show('Перехід на ' + item.title);
-                    localStorage.setItem('host', item.url);
-                    setTimeout(function () {
-                        location.href = item.url;
-                    }, 500);
+                title: 'Оберіть сервер',
+                items: srv,
+                onSelect: function (i) {
+                    localStorage.setItem('host', i.url);
+                    Lampa.Noty.show('Перехід на ' + i.title);
+                    setTimeout(function () { location.href = i.url; }, 500);
                 },
-                onBack: function () {
-                    Lampa.Controller.toggle('menu');
-                }
+                onBack: function () { Lampa.Controller.toggle('menu'); }
             });
         };
-
-        // Створення кнопки
-        var btn = $('<li class="menu__item selector"><div class="menu__ico"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><rect x="4" y="4" width="16" height="16" rx="2" stroke="white" stroke-width="2"/></svg></div><div class="menu__text">Змінити хост</div></li>');
-        
-        btn.on('hover:enter', function () {
-            openMenu();
-        });
-
-        // Додаємо в меню з невеликою затримкою для надійності
-        setTimeout(function() {
-            $('.menu .menu__list').append(btn);
-        }, 1000);
+        var item = $('<li class="menu__item selector"><div class="menu__ico"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4 12h16M4 6h16M4 18h16" stroke="white" stroke-width="2" stroke-linecap="round"/></svg></div><div class="menu__text">ЗМІНИТИ СЕРВЕР</div></li>');
+        item.on('hover:enter', open);
+        $('.menu .menu__list').append(item);
     }
-
     if (window.appready) start();
     else Lampa.Listener.follow('app', function (e) { if (e.type == 'ready') start(); });
 })();
